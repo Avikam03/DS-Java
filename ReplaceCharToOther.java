5b5ab6637d75f790fce9b4a6c17265826f14e576
deepanshu464 <gupta96.shivam@gmail.com>
2017-07-14 16:41:01 +0530

    modify again

--- ReplaceCharToOther.java
+++ ReplaceCharToOther.java
@@ -1,21 +1,73 @@
-//Replace a character in a string with another character.
+////Replace a character in a string with another character.
+//package practice;
+//
+//import java.util.Arrays;
+//
+//public class ReplaceCharToOther {
+//    public static void main(String[] args) {
+//        StringBuffer str_new = replaceChar("Java is Great", ' ', "%20");
+//        System.out.println(str_new);
+//    }
+//
+//    private static StringBuffer replaceChar(String str, char ch1, String ch2) {
+//        StringBuffer sb = new StringBuffer();
+//        char[] ch_arr = str.toCharArray();
+//        for(int i=0;i<str.length();i++){
+//            if(ch_arr[i] == ch1){
+//                sb.append(ch2);
+//            }
+//            else{
+//                sb.append(ch_arr[i]);
+//            }
+//        }
+//        return sb;
+//    }
+//
+//}
+
+
+//package practice;
+//
+//import java.util.Arrays;
+//
+//class ReplaceCharToOther{
+//    public static void main(String[] args) {
+//        String str = "Java is great";
+//        String[] str_array = str.split(" ");
+//        StringBuffer sb = new StringBuffer();
+//        for(int i=0;i<str_array.length;i++){
+//            if(i == str_array.length-1){
+//                sb = sb.append(str_array[i]);
+//                break;
+//            }
+//            sb = sb.append(str_array[i] + "Shivam");
+//        }
+//        System.out.println(sb);
+//    }
+//}
+
+
+
 package practice;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
-public class ReplaceCharToOther {
+class ReplaceCharToOther{
     public static void main(String[] args) {
-        System.out.println(replaceChar("Java is Great", ' ', "%20"));
-    }
-
-    private static String replaceChar(String str, char ch1, String ch2) {
-        char[] char_array = str.toCharArray();
-        for(int i=0;i<str.length();i++){
-            Character character = str.charAt(i);
-            if(character.toString() == ch1){
-                char_array[i] = ch2;
+        String str = "Java is great";
+        String[] str_array = str.split(" ");
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i=0;i<str_array.length;i++){
+            if(i == str_array.length-1){
+                list.add(str_array[i]);
+                break;
             }
+            list.add(str_array[i] + "Shivam");
         }
-        return String.valueOf(char_array);
+        String answer = "";
+        for(String s : list){
+            answer = answer + s + "";
+        }
+        System.out.println(answer);
     }
 }
--- ReverseWordsInString.java
+++ ReverseWordsInString.java
@@ -2,17 +2,16 @@ package practice;
 
 public class ReverseWordsInString {
     public static void main(String[] args) {
-        System.out.println(reverseWords("This is me"););
+        System.out.println(reverseWords("This is me"));
     }
 
-    private static String reverseWords(String str) {
+    private static StringBuffer reverseWords(String str) {
         String[] str_array = str.split(" ");
-        for(int i=str_array.length-1;i>=0;i++){
-            String string = "";
-            char[] char_array = str_array[i].toCharArray();
-            for(int j=char_array.length-1;j>=0;j--){
-                string = string + char_array[j];
-            }
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<str_array.length;i++){
+            sb.append(str_array[i]).reverse();
+            sb.append(" ");
         }
+        return sb;
     }
 }
