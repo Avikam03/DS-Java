b83a06c7f50d8b77f636e9ebce6f6262d4c7e43c
deepanshu464 <gupta96.shivam@gmail.com>
2017-08-02 01:14:19 +0530

    Pattern Programs again

---
+++ Codezen(Patterns)/NumberPattern.java
@@ -0,0 +1,20 @@
+/*
+    333
+    233
+    123
+*/
+package Patterns;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++){
+            for(int j=3-i;j<3;j++){
+                System.out.print(j);
+            }
+            for(int k=3;k>i;k--){
+                System.out.print("3");
+            }
+            System.out.println();
+        }
+    }
+}
